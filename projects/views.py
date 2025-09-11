11eb6438a34e223d09d21e11c63b7f9a04f43808
Alun Price <alunprice089@gmail.com>
2025-09-11 21:37:53 +0100

    Initial devsearch commit with Models Project, Review and Tags

---
+++ projects/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from .models import Project
+
+
+# Create your views here.
+def projects(request):
+    projects = Project.objects.all()
+    context = {"projects": projects}
+    return render(request, "projects/projects.html", context)
+
+
+def project(request, pk):
+    projectObj = Project.objects.get(id=pk)
+    return render(request, "projects/single-project.html", {"project": projectObj})
